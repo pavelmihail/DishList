cf2ca54628693a900f69444a054b7cbaa67c6802
Mihai <valmi2009@gmail.com>
2022-08-21 23:21:24 +0300

    Setting up Entity and DAO

---
+++ app/src/main/java/com/practice/dishlist/model/database/DishListDAO.kt
@@ -0,0 +1,12 @@
+package com.practice.dishlist.model.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import com.practice.dishlist.model.entities.DishList
+
+@Dao
+interface DishListDAO {
+
+    @Insert
+    suspend fun insertFishListDetails(dishList: DishList)
+}

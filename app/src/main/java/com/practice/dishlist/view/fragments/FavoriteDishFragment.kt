5a7721f8e1d30d921a8398c2b4f2d6bf82e286ab
Mihai <valmi2009@gmail.com>
2022-09-02 13:46:29 +0300

    Small fixes

---
+++ app/src/main/java/com/practice/dishlist/view/fragments/FavoriteDishFragment.kt
@@ -0,0 +1,25 @@
+package com.practice.dishlist.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.practice.dishlist.viewmodel.DashboardViewModel
+
+class FavoriteDishFragment : Fragment() {
+
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
